a96d60c3ed05549f268ea80f37d35a444e60fe2a
Mitchell Hudson <soggybag@gmail.com>
2025-06-11 17:33:32 -0700

    Initial Commit

---
+++ SwiftUI-Relic-Generator/SwiftUI_Relic_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Relic_GeneratorApp.swift
+//  SwiftUI-Relic-Generator
+//
+//  Created by mitchell hudson on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Relic_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
